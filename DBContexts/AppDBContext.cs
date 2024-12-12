59211b90a5608028d6989b62aa018244dc52dc92
KSH951007 <katafi6@naver.com>
2024-12-13 01:18:13 +0900

    초기 커밋.

---
+++ DBContexts/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿
+using FantasyKnightWebServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDBContext : DbContext
+{
+    public DbSet<UserAccountDBData> users { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            "Server=127.0.0.1;Database=FantasyKnightDB;User=root;Password=3900;",
+            new MySqlServerVersion(new Version(8, 0, 29))
+        );
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserAccountDBData>().HasKey(user => user.uuid);
+    }
+}
+
---
+++ Models/UserAccountDBData.cs
@@ -0,0 +1,12 @@
+﻿namespace FantasyKnightWebServer.Models
+{
+    public class UserAccountDBData
+    {
+        public int uuid { get; set; }
+        public string nicName { get; set; }
+
+        public DateTime createdAt { get; set; }
+        public DateTime lastedAt { get; set; }
+
+    }
+}
